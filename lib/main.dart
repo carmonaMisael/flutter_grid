34f5e3aab9a767a1b53bd2b539876c5786171f3e
MISAEL_CARMONA_SANTANA <a.20308051280485@cbtis128.edu.mx>
2023-04-24 14:18:11 +0000

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,124 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter App Learning',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          title: Text("Heladeria"),
         ),
-      ),
-    );
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: [
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen4.jpeg')),
+                  Text("Paletas", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen2.jpeg')),
+                  Text("Bote Helado", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen3.png')),
+                  Text("Conos", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen4.jpeg')),
+                  Text("Paletas", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen5.jpg')),
+                  Text("Equipo", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagenn.png')),
+                  Text("Helado", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen10.jpg')),
+                  Text("Bola de helado", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(image: AssetImage('assets/images/imagen11.png')),
+                  Text("Malteada", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+          ],
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ));
   }
 }
